5105ff38cb8b9c41f787549e9c0bc45fca3e8548
James Rees <James.Rees@sigma-systems.com>
2016-01-07 15:40:59 +0000

    Clean for merge

--- public/scripts/Controllers/blogController.js
+++ public/scripts/Controllers/blogController.js
@@ -5,13 +5,17 @@ var Blog;
     var blogController = (function () {
         function blogController($scope, $http) {
             this.$scope = $scope;
+            this.$http = $http;
             $scope.OutsideFunction = this.OutsideFunction;
             $scope.greetingText = "Hello, Typescripty world!";
+            $scope.update = this.update;
             //   this.OutsideFunction();
         }
         blogController.prototype.OutsideFunction = function () {
             console.log("found function!");
             this.$scope.greetingText = "This can be found when using controller as!";
+        };
+        blogController.prototype.update = function () {
         };
         return blogController;
     })();
--- public/scripts/Controllers/blogController.ts
+++ public/scripts/Controllers/blogController.ts
@@ -3,18 +3,30 @@
 module Blog {
     export interface IBlogScope {
            greetingText: string;
+           currentUrlText: string;
            OutsideFunction(): void;
+           update(): void;
+           callroute(): void;
     }   
 
     export class blogController {
-        constructor(public $scope: IBlogScope, $http) {
+   
+               
+        constructor(public $scope: IBlogScope, public $http: ng.IHttpService) {
             $scope.OutsideFunction = this.OutsideFunction;
             $scope.greetingText = "Hello, Typescripty world!";     
+            
+            
          //   this.OutsideFunction();
         }
         OutsideFunction(){
             console.log("found function!");
             this.$scope.greetingText = "This can be found when using controller as!";
         }
+        
+        CallRoute(){
+    
+        }
+        
     }
 }
