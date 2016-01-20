cc43a2007b11982d719ed09283ef2aca56ed7aea
James Rees <James.Rees@sigma-systems.com>
2016-01-20 16:36:35 +0000

    Added more unit tests for unwritten functionality to be added in later

--- Atsb-server-side/DataAccess/test/blogManagerTest.js
+++ Atsb-server-side/DataAccess/test/blogManagerTest.js
@@ -25,11 +25,22 @@ describe("Blog Manager", function () {
     });
     describe("When addContent() is supplied with a JSON string", function () {
         it("Should write to the database", function () {
-            return false;
+            //ToDo: write a pre-hook that connects to a dummy collection in the database
+            //then perform a read after writing, output should be equal to data entered.
+            //ToDo: write a post-hook to destroy the database
         });
     });
     describe("When removeContent() is supplied with the Blog post ID", function () {
         it("Should remove the blog post from the database", function () {
+            //ToDo: write a pre-hook that connects to a dummy collection in the database
+            //then perform a search for the blog post after entry, should fail
+            //ToDo: write a post-hook to destroy the database
+        });
+    });
+    describe("When blogCount() is called", function () {
+        it("Should return a value of how many entries are in the blog database", function () {
+            //ToDo: create a pre-hook that creates a dummy database with 10 posts
+            //ToDo: destroy database in posthook
         });
     });
 });
